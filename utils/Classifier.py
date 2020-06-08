37fc96bfd67e6de98f38cc509bea9a08e87702e9
abdullahtarek <abdullahtarek@rocketmail.com>
2020-06-08 23:48:02 +0200

    Add classifier

---
+++ utils/Classifier.py
@@ -0,0 +1,24 @@
+from .ModelBlock import ModelBlock
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Dense
+
+
+class Classifier(ModelBlock):
+    def __init__(self, encoder):
+        self.input_layer = encoder.model.output
+        self.model = self.make_model()
+
+    def make_model(self):
+        """
+        This model is responsible for building a keras model
+        :return:
+            keras model:
+        """
+
+        X = Flatten()(self.input_layer)
+        X = Dense(3, activation='softmax')(X)
+
+        return X
