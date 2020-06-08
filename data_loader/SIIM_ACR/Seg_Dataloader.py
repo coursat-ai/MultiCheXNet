eaace844f07c32e7489842611291516129b0aad3
Abdullah Tarek <abdullahtarek@rocketmail.com>
2020-06-08 21:38:58 +0200

    edit segmentation_data_loader

--- data_loader/SIIM_ACR/Seg_Dataloader.py
+++ data_loader/SIIM_ACR/Seg_Dataloader.py
@@ -5,21 +5,97 @@ import pandas as pd
 from glob import glob
 import pydicom
 import cv2
-import keras
-from keras.applications.vgg16 import preprocess_input
-from MultiCheXNet.data_loader.SIIM_ACR.Seg_manipulation import rle2mask
+import tensorflow.keras as keras
+from tensorflow.keras.applications.vgg16 import preprocess_input
+import tensorflow
+from skimage.transform import resize
 
-class Seg_gen(keras.utils.Sequence):
+def process_labels(df): #Converts labels to binary
+    
+    labels = df[" EncodedPixels"].to_list()
+    a = []
+    for i in labels:
+        if i == ' -1':
+            a.append(0)
+        else: a.append(1)
+    df["Class"] = np.array(a, dtype='uint8')
+    return df
+
+def add_full_path(df, train_path):
+    my_glob = glob(train_path + '/*/*/*.dcm')
+
+    full_img_paths = {os.path.basename(x).split('.dcm')[0]: x for x in my_glob}
+    dataset_path = df['ImageId'].map(full_img_paths.get)
+    
+    df['full_path'] = dataset_path
+    
+    return df
+
+
+def mask2rle(img, width, height):
+    rle = []
+    lastColor = 0;
+    currentPixel = 0;
+    runStart = -1;
+    runLength = 0;
+
+    for x in range(width):
+        for y in range(height):
+            currentColor = img[x][y]
+            if currentColor != lastColor:
+                if currentColor == 255:
+                    runStart = currentPixel;
+                    runLength = 1;
+                else:
+                    rle.append(str(runStart));
+                    rle.append(str(runLength));
+                    runStart = -1;
+                    runLength = 0;
+                    currentPixel = 0;
+            elif runStart > -1:
+                runLength += 1
+            lastColor = currentColor;
+            currentPixel+=1;
+
+    return " ".join(rle)
+
+def rle2mask(rle, width, height):
+    if rle == ' -1':
+        rle ='0 0'
+    
+    mask= np.zeros(width* height)
+    array = np.asarray([int(x) for x in rle.split()])
+    starts = array[0::2]
+    lengths = array[1::2]
+
+    current_position = 0
+    for index, start in enumerate(starts):
+        current_position += start
+        mask[current_position:current_position+lengths[index]] = 255
+        current_position += lengths[index]
+
+    return mask.reshape(width, height)
+
+
+class Seg_gen(tensorflow.keras.utils.Sequence):
     'Generates data from a Dataframe'
-    def __init__(self, df, preprocess_fct, batch_size=32, dim=(1024, 1024), shuffle=True):
+    def __init__(self, df_path,train_path, preprocess_fct, batch_size=32, dim=(1024, 1024), shuffle=True):
         'Initialization'
+        
+        train_filenames = os.listdir(train_path)
+        rle_csv = pd.read_csv(df_path)
+        
+        rle_csv = process_labels(rle_csv)
+        self.df = add_full_path(rle_csv, train_path)
+        
+        
+        
         self.preprocess_fct = preprocess_fct
         self.dim = dim
         self.batch_size = batch_size
         self.shuffle = shuffle
 
-        self.df = df
-        self.n = len(df)            
+        self.n = len(self.df)            
         self.nb_iteration = int(np.floor(self.n  / self.batch_size))
         
         self.on_epoch_end()
@@ -56,11 +132,20 @@ class Seg_gen(keras.utils.Sequence):
             # Read the image
             img  = pydicom.dcmread(self.df['full_path'][ID]).pixel_array
             mask = rle2mask(self.df[' EncodedPixels'][ID], *img.shape).T/255           
-                                                                                
+            
             img  = np.asarray(cv2.resize(img, self.dim))
-            mask = np.asarray(cv2.resize(mask, self.dim))            
+            #mask = np.asarray(cv2.resize(mask, self.dim, interpolation = cv2.INTER_AREA ))    
+
+            mask= resize(mask,
+                               self.dim,
+                               mode='edge',
+                               anti_aliasing=False,
+                               anti_aliasing_sigma=None,
+                               preserve_range=True,
+                               order=0)
+            
             
             X[i,] = np.asarray(img)#self.preprocess_fct(np.asarray(img))
             Y[i,] = np.asarray(mask)
         
-        return X, Y 
+        return X, Y
