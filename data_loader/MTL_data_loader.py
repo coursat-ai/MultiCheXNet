db07f748dacbc3a47d9fcebfddff8bd2cec53007
abdullahtarek <abdullahtarek@rocketmail.com>
2020-06-13 20:57:10 +0200

    make training and validation dataloaders gernerator function

--- data_loader/MTL_data_loader.py
+++ data_loader/MTL_data_loader.py
@@ -2,35 +2,18 @@ import tensorflow
 
 
 import numpy as np
-from tensorflow.keras.applications.vgg16 import preprocess_input
-from .chex_xray_14 import Data_generator as Chex_gen
-from .SIIM_ACR import Seg_Dataloader as Seg_gen
+from .SIIM_ACR_dataloader import get_train_validation_generator as segmenatation_get_train_validation_generator
+from .RSNA_dataloader import get_train_validation_generator as detection_get_train_validation_generator
 
 class MTL_generatot(tensorflow.keras.utils.Sequence):
     'Generates data from a Dataframe'
 
-    def __init__(self, df_seg_path, seg_data_folder, df_det, batch_size=8, dim=(256, 256), shuffle=True):
+    def __init__(self, Segmentation_gen, detection_gen , nb_iteration):
         'Initialization'
-        self.dim = dim
-        self.batch_size = batch_size
-        self.shuffle = shuffle
 
-        self.df_seg = df_seg_path
-        # TF 100%
-        self.df_seg = self.df_seg[self.df_seg['Class'] != 0].reset_index()
-        # TF 100%
-        self.df_det = df_det
-        # self.df_det  = self.df_det[self.df_det['Class_Det']!=0].reset_index()
-
-        self.seg_generator = Seg_gen.Seg_gen(self.df_seg,seg_data_folder, preprocess_input, batch_size=self.batch_size, dim=self.dim,
-                                             shuffle=True)
-        self.det_generator = Chex_gen.Data_gen(self.df_det, preprocess_input, batch_size=self.batch_size, dim=self.dim,
-                                               shuffle=True)
-
-        self.n = len(self.df_seg) + len(self.df_det)
-
-        self.nb_iteration = int(np.floor(self.n / self.batch_size))
-        self.on_epoch_end()
+        self.seg_generator = Segmentation_gen
+        self.det_generator = detection_gen
+        self.nb_iteration = nb_iteration
         self.batch_number = 0
 
     def __len__(self):
@@ -47,12 +30,6 @@ class MTL_generatot(tensorflow.keras.utils.Sequence):
 
         return X, y
 
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.df_seg) + len(self.df_det))
-        if self.shuffle == True:
-            np.random.shuffle(self.indexes)
-
     def __data_generation(self, index):
         'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
         # Initialization
@@ -64,9 +41,36 @@ class MTL_generatot(tensorflow.keras.utils.Sequence):
 
         else:
             X, Y_det = next(iter(self.det_generator))
-            Y_class = [[0, 1, 0]] * self.batch_size
+            Y_class = [[0, 0, 1]] * self.batch_size
             Y_seg = None
 
         self.batch_number += 1
 
         return X, np.array([np.array(Y_class), np.array(Y_det), np.array(Y_seg)])
+
+def get_train_validation_generator(det_csv_path,seg_csv_path , det_img_path, seg_img_path ,batch_size=8, dim=(256,256), n_channels=3,
+                  shuffle=True ,preprocess = None , only_positive=True, validation_split=0.2 ):
+
+
+
+
+    seg_train_gen, seg_valid_gen = segmenatation_get_train_validation_generator(seg_csv_path, seg_img_path,
+                                                                                batch_size=batch_size, dim=dim,
+                                                                                n_channels=n_channels, shuffle=shuffle,
+                                                                                preprocess=preprocess,
+                                                                                only_positive=only_positive,
+                                                                                validation_split=validation_split)
+
+    det_train_gen, det_valid_gen = detection_get_train_validation_generator(det_csv_path, det_img_path,
+                                                                                batch_size=batch_size, dim=dim,
+                                                                                n_channels=n_channels, shuffle=shuffle,
+                                                                                preprocess=preprocess,
+                                                                                only_positive=only_positive,
+                                                                                validation_split=validation_split)
+
+    MTL_train_gen = MTL_generatot(seg_train_gen, det_train_gen , seg_train_gen.nb_iteration+det_train_gen.nb_iteration)
+    MTL_valid_gen = MTL_generatot(seg_valid_gen, det_valid_gen, seg_valid_gen.nb_iteration + det_valid_gen.nb_iteration)
+
+    return MTL_train_gen, MTL_valid_gen
+
+
