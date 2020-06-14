81eb7ecb89675f8959bb33b9bdad04bd1aab2128
Omar Sayed <sayedomar74@gmail.com>
2020-06-14 21:27:23 +0200

    update to work with TF 2.0

--- utils/compute_flops.py
+++ utils/compute_flops.py
@@ -1,10 +1,17 @@
-import tensorflow as tf
-def get_flops(model):
-    run_meta = tf.compat.v1.RunMetadata()
-    opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
+def get_flops(model_h5_path):
+    session = tf.compat.v1.Session()
+    graph = tf.compat.v1.get_default_graph()
+    #graph = tf.compat.v1.reset_default_graph()    
+    with graph.as_default():
+        with session.as_default():
+            
+            model = tf.keras.models.load_model(model_h5_path)
 
-    # We use the Keras session graph in the call to the profiler.
-    flops = tf.compat.v1.profiler.profile(graph=tf.compat.v1.keras.backend.get_session().graph,
-                                run_meta=run_meta, cmd='op', options=opts)
-
-    return flops.total_float_ops  # Prints the "flops" of the model.
+            run_meta = tf.compat.v1.RunMetadata()
+            opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
+        
+            # We use the Keras session graph in the call to the profiler.
+            flops = tf.compat.v1.profiler.profile(graph=graph,
+                                                  run_meta=run_meta, cmd='op', options=opts)
+        
+            return flops.total_float_ops
