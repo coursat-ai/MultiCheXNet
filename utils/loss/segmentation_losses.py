889ac0bea9385e872cb26fca9f788aae83df827a
Omar Sayed <sayedomar74@gmail.com>
2020-06-14 20:58:33 +0200

    adding working dice loss functions.
    Abdullah tested it and it works.

--- utils/loss/segmentation_losses.py
+++ utils/loss/segmentation_losses.py
@@ -1,14 +1,17 @@
 from keras import backend as K
 
 #refrence: https://gist.github.com/wassname/7793e2058c5c9dacb5212c0ac0b18a8a
-def dice_coef(y_true, y_pred, smooth=1):
-    """
-    Dice = (2*|X & Y|)/ (|X|+ |Y|)
-         =  2*sum(|A*B|)/(sum(A^2)+sum(B^2))
-    ref: https://arxiv.org/pdf/1606.04797v1.pdf
-    """
-    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
-    return (2. * intersection + smooth) / (K.sum(K.square(y_true),-1) + K.sum(K.square(y_pred),-1) + smooth)
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
 
-def dice_coef_loss(y_true, y_pred):
-    return 1-dice_coef(y_true, y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (200. * K.sum(intersection) + smooth) / (100. * K.sum(y_true_f) + 100.* K.sum(y_pred_f) + smooth)
+    return  (1. - score)
+
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+
+def bce_logdice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) - K.log(1. - dice_loss(y_true, y_pred))
