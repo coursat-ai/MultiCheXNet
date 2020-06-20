9e78d702f99ccb6685824b5d49ae5a76fdca38f8
Abdullah Tarek <abdullahtarek@rocketmail.com>
2020-06-20 17:50:01 +0200

    Add focal loss to segmentation

--- utils/Segmenter.py
+++ utils/Segmenter.py
@@ -8,6 +8,7 @@ class Segmenter(ModelBlock):
     def __init__(self, encoder):
         self.encoder = encoder.model
         self.model = self.make_model()
+        self.focal_loss= binary_focal_loss()
         #self.num_layers = ModelBlock.get_head_num_layers(encoder, self.model)
 
     def dense_block(self, x, blocks, name):
@@ -86,9 +87,37 @@ class Segmenter(ModelBlock):
     def loss(self,y_true,y_pred):
         if tf.math.reduce_all(tf.math.equal(y_true,-1)):
             return  tf.convert_to_tensor(0, dtype=tf.float32)
-        return dice_loss(y_true, y_pred)
+        return self.focal_loss(y_true, y_pred)
 
 
+def binary_focal_loss(gamma=2., alpha=.25):
+    """
+    Binary form of focal loss.
+      FL(p_t) = -alpha * (1 - p_t)**gamma * log(p_t)
+      where p = sigmoid(x), p_t = p or 1 - p depending on if the label is 1 or 0, respectively.
+    References:
+        https://arxiv.org/pdf/1708.02002.pdf
+    Usage:
+     model.compile(loss=[binary_focal_loss(alpha=.25, gamma=2)], metrics=["accuracy"], optimizer=adam)
+    """
+    def binary_focal_loss_fixed(y_true, y_pred):
+        """
+        :param y_true: A tensor of the same shape as `y_pred`
+        :param y_pred:  A tensor resulting from a sigmoid
+        :return: Output tensor.
+        """
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+
+        epsilon = K.epsilon()
+        # clip to prevent NaN's and Inf's
+        pt_1 = K.clip(pt_1, epsilon, 1. - epsilon)
+        pt_0 = K.clip(pt_0, epsilon, 1. - epsilon)
+
+        return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) \
+               -K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
+
+    return binary_focal_loss_fixed
         
 #refrence: https://gist.github.com/wassname/7793e2058c5c9dacb5212c0ac0b18a8a
 def dice_loss(y_true, y_pred):
