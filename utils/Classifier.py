158ccf42e1c3194562ab6795e23bf8f0f219f6bb
Reem <Roooka219@gmail.com>
2020-06-27 05:08:34 +0200

    Add classification model layers and loss.

--- utils/Classifier.py
+++ utils/Classifier.py
@@ -1,27 +1,34 @@
 from .ModelBlock import ModelBlock
-from tensorflow.keras.layers import Flatten,Input,Dense, GlobalAveragePooling2D , Dropout
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.layers import *
+from tensorflow.keras.losses import BinaryCrossentropy
+
 
 class Classifier(ModelBlock):
     def __init__(self, encoder):
+        super().__init__()
         self.encoder_output = encoder.model.output
         self.model = self.make_model()
         self.num_layers = ModelBlock.get_head_num_layers(encoder, self.model)
 
     def make_model(self):
         """
-        This model is responsible for building a keras model
+        This model is responsible for building a keras model that can classify ChestXRay data-set.
         :return:
             keras model:
         """
 
-        X = GlobalAveragePooling2D()(self.encoder_output)
-        X = Dropout(0.2)(X)
-        X = Dense(256, activation='relu' , activity_regularizer=l2(0.01))(X)
-        X = Dropout(0.2)(X)
-        X = Dense(3, activation='softmax')(X)
+        x = GlobalAveragePooling2D()(self.encoder_output)
 
-        return X
-    def loss(self,y_true,y_pred):
-        return categorical_crossentropy(y_true,y_pred)
+        x = BatchNormalization()(x)
+        x = Dropout(0.25)(x)
+        x = Dense(512, activation='relu')(x)
+        x = Dense(256, activation='relu')(x)
+        x = BatchNormalization()(x)
+        x = Dropout(0.25)(x)
+
+        output_layer = Dense(15, activation='sigmoid')(x)
+
+        return output_layer
+
+    def loss(self, y_true, y_pred):
+        return BinaryCrossentropy(y_true, y_pred)
