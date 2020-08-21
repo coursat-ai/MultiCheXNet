f2d58f07521a99ea59ba81dfb0f4752a3492ea2c
Abdullah Tarek <abdullahtarek@rocketmail.com>
2020-08-21 14:47:43 +0200

    Create text_cleaner.py

---
+++ utils/text_cleaner.py
@@ -0,0 +1,95 @@
+import pandas as pd 
+import cv2
+import numpy as np
+import os
+from glob import glob
+import math
+import matplotlib.pyplot as plt
+
+import re
+import html
+import string
+import unicodedata
+from nltk.tokenize import word_tokenize
+
+
+def remove_special_chars(text):
+    re1 = re.compile(r'  +')
+    x1 = text.lower().replace('#39;', "'").replace('amp;', '&').replace('#146;', "'").replace(
+        'nbsp;', ' ').replace('#36;', '$').replace('\\n', "\n").replace('quot;', "'").replace(
+        '<br />', "\n").replace('\\"', '"').replace('<unk>', 'u_n').replace(' @.@ ', '.').replace(
+        ' @-@ ', '-').replace('\\', ' \\ ')
+    return re1.sub(' ', html.unescape(x1))
+
+
+def remove_non_ascii(text):
+    """Remove non-ASCII characters from list of tokenized words"""
+    return unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+
+
+def to_lowercase(text):
+    return text.lower()
+
+def remove_punctuation(text):
+    """Remove punctuation from list of tokenized words"""
+    translator = str.maketrans('', '', string.punctuation)
+    return text.translate(translator)
+
+
+def replace_numbers(text):
+    """Replace all interger occurrences in list of tokenized words with textual representation"""
+    return re.sub(r'\d+', '', text)
+
+
+def remove_whitespaces(text):
+    return text.strip()
+
+
+def remove_stopwords(words, stop_words):
+    """
+    :param words:
+    :type words:
+    :param stop_words: from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+    or
+    from spacy.lang.en.stop_words import STOP_WORDS
+    :type stop_words:
+    :return:
+    :rtype:
+    """
+    return [word for word in words if word not in stop_words]
+
+
+def stem_words(words):
+    """Stem words in text"""
+    stemmer = PorterStemmer()
+    return [stemmer.stem(word) for word in words]
+
+def lemmatize_words(words):
+    """Lemmatize words in text"""
+
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(word) for word in words]
+
+def lemmatize_verbs(words):
+    """Lemmatize verbs in text"""
+
+    lemmatizer = WordNetLemmatizer()
+    return ' '.join([lemmatizer.lemmatize(word, pos='v') for word in words])
+
+def text2words(text):
+    return word_tokenize(text)
+
+def normalize_text(text):
+    text = remove_special_chars(text)
+    text = remove_non_ascii(text)
+    text = remove_punctuation(text)
+    text = to_lowercase(text)
+    text = replace_numbers(text)
+    #words = text2words(text)
+    #stop_words = stopwords.words('english')
+    #words = remove_stopwords(words, stop_words)
+    #words = stem_words(words)# Either stem ovocar lemmatize
+    #words = lemmatize_words(words)
+    #words = lemmatize_verbs(words)
+
+    return text
