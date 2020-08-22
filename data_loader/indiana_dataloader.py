f9b4c4b0fc01d05c9fb01800aaeaeafe88454a46
Abdullah Tarek <abdullahtarek@rocketmail.com>
2020-08-22 11:24:54 +0200

    add start and end tokens

--- data_loader/indiana_dataloader.py
+++ data_loader/indiana_dataloader.py
@@ -85,7 +85,7 @@ def get_train_validation_generator(csv_path1,csv_path2,img_path, vocab_size,max_
     
     df= df.dropna(subset=['findings'])
     df['findings_cleaned'] = df['findings'].apply(normalize_text)
-    
+    df['findings_cleaned'] = 'startseq '+df['findings_cleaned']+' endseq'
     
     vocab_size = vocab_size
     max_len = max_len
