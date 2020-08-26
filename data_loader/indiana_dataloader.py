41d5367cb5e6c04d8cff4a6c619c943d96f794c4
Abdullah Tarek <abdullahtarek@rocketmail.com>
2020-08-26 22:39:29 +0200

    Ad image augmentations

--- data_loader/indiana_dataloader.py
+++ data_loader/indiana_dataloader.py
@@ -6,17 +6,46 @@ import numpy as np
 import cv2
 import math
 import os
-from .text_cleaner import normalize_text
+from skimage import exposure
+#from .text_cleaner import normalize_text
+from MultiCheXNet.data_loader.text_cleaner import normalize_text #Change with the commented import up
+
+from albumentations import (
+    Compose, HorizontalFlip, CLAHE, HueSaturationValue,
+    RandomBrightness, RandomContrast, RandomGamma,OneOf,
+    ToFloat, GridDistortion, ElasticTransform, OpticalDistortion, 
+    RandomSizedCrop,Rotate
+)
+
+h,w = 256,256
+AUGMENTATIONS_TRAIN = Compose([
+    HorizontalFlip(p=0.5),
+    OneOf([
+        
+        RandomBrightness(),
+         ], p=0.3),
+    OneOf([
+        ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
+        GridDistortion(),
+        OpticalDistortion(distort_limit=2, shift_limit=0.5),
+        ], p=0.3),
+    Rotate(limit=15, p=0.2),
+    RandomSizedCrop(min_max_height=(176, 256), height=h, width=w,p=0.25)],p=1)
+
 
 class det_gen(tensorflow.keras.utils.Sequence):
     'Generates data from a Dataframe'
-    def __init__(self,df, tok, max_len,images_path, dim=(256,256), batch_size=8,preprocess_func=None):
+    def __init__(self,df, tok, max_len,images_path, dim=(256,256), batch_size=8,preprocess_func=None,hist_eq=False,normalize=False,augmentation=False):
         self.df=df
         self.dim = dim
         self.images_path = images_path
         self.tok= tok
         self.max_len = max_len
         self.batch_size = batch_size
+        self.hist_eq = hist_eq
+        self.normalize=normalize
+        self.augmentation = augmentation
+        
         self.nb_iteration = math.ceil((self.df.shape[0])/self.batch_size)
         self.preprocess_func = preprocess_func
 
@@ -34,6 +63,17 @@ class det_gen(tensorflow.keras.utils.Sequence):
         img =cv2.resize(img,(self.dim))
         if self.preprocess_func is not None:
             img=self.preprocess_func(img)
+        
+        if self.augmentation == 'train':
+            aug= AUGMENTATIONS_TRAIN(image=img)
+            img=aug['image']
+        
+        
+        if self.hist_eq:
+            img= exposure.equalize_adapthist(img)
+            
+        if self.normalize and img.max()>1:
+            img=np.array(img,np.float32)/255
             
         return img
         
@@ -76,7 +116,7 @@ class det_gen(tensorflow.keras.utils.Sequence):
         return [np.array(images), np.array(x_batch_input)] , np.array(x_batch_gt)   
 
 
-def get_train_validation_generator(csv_path1,csv_path2,img_path, vocab_size,max_len,batch_size=8, dim=(256,256),shuffle=True ,preprocess = None , validation_split=0.2,augmentation=False,normalize=False,hist_eq =False,):
+def get_train_validation_generator(csv_path1,csv_path2,img_path, vocab_size,max_len,batch_size=8, dim=(256,256),shuffle=True ,preprocess = None , validation_split=0.2,augmentation=False,normalize=False,hist_eq =False):
     
     df1= pd.read_csv(csv_path1)
     df2= pd.read_csv(csv_path2)
@@ -101,8 +141,15 @@ def get_train_validation_generator(csv_path1,csv_path2,img_path, vocab_size,max_
     df_train = df.iloc[:-int(df.shape[0]*validation_split)]
     df_val   = df.iloc[-int(df.shape[0]*validation_split):]
     
-    train_dataloader =  det_gen(df_train, tok, max_len,img_path,dim=dim,batch_size=batch_size,preprocess_func=preprocess )
-    val_dataloader =  det_gen(df_val, tok, max_len,img_path,dim=dim,batch_size=batch_size,preprocess_func=preprocess )
+    if augmentation == True:
+        augmentation='train'
+        
+    train_dataloader =  det_gen(df_train, tok, max_len,img_path,dim=dim,batch_size=batch_size,preprocess_func=preprocess,normalize=normalize,hist_eq=hist_eq,augmentation=augmentation  )
+    
+    if augmentation == 'train':
+        augmentation='validation'
+    
+    val_dataloader =  det_gen(df_val, tok, max_len,img_path,dim=dim,batch_size=batch_size,preprocess_func=preprocess,normalize=normalize,hist_eq=hist_eq,augmentation=augmentation  )
     
 
     return train_dataloader, val_dataloader, vocab_size, tok
