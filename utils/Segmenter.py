6c94e4a3e0ca6f7e5d5d5681a321dcd284443040
Abdullah Tarek <abdullahtarek@rocketmail.com>
2020-10-29 17:11:55 +0200

    update segmenter loss function

--- utils/Segmenter.py
+++ utils/Segmenter.py
@@ -91,44 +91,16 @@ class Segmenter(ModelBlock):
         return dice_loss(y_true, y_pred)
 
 
-def binary_focal_loss(gamma=2., alpha=.25):
-    """
-    Binary form of focal loss.
-      FL(p_t) = -alpha * (1 - p_t)**gamma * log(p_t)
-      where p = sigmoid(x), p_t = p or 1 - p depending on if the label is 1 or 0, respectively.
-    References:
-        https://arxiv.org/pdf/1708.02002.pdf
-    Usage:
-     model.compile(loss=[binary_focal_loss(alpha=.25, gamma=2)], metrics=["accuracy"], optimizer=adam)
-    """
-    def binary_focal_loss_fixed(y_true, y_pred):
-        """
-        :param y_true: A tensor of the same shape as `y_pred`
-        :param y_pred:  A tensor resulting from a sigmoid
-        :return: Output tensor.
-        """
-        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-
-        epsilon = K.epsilon()
-        # clip to prevent NaN's and Inf's
-        pt_1 = K.clip(pt_1, epsilon, 1. - epsilon)
-        pt_0 = K.clip(pt_0, epsilon, 1. - epsilon)
-
-        return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) \
-               -K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
-
-    return binary_focal_loss_fixed
         
 #refrence: https://gist.github.com/wassname/7793e2058c5c9dacb5212c0ac0b18a8a
-def dice_loss(y_true, y_pred):
-    smooth = 1.
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
+def dice_coeff(y_true, y_pred, smooth=1):
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    return (2. * intersection + smooth) / (K.sum(K.square(y_true),-1) + K.sum(K.square(y_pred),-1) + smooth)
 
-    intersection = y_true_f * y_pred_f
-    score = (200. * K.sum(intersection) + smooth) / (100. * K.sum(y_true_f) + 100.* K.sum(y_pred_f) + smooth)
-    return  (1. - score)
+@tf.function
+def dice_loss(y_true, y_pred):
+    loss= 1-dice_coeff(y_true, y_pred)
+    return loss 
 
 def bce_dice_loss(y_true, y_pred):
     return BinaryCrossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
