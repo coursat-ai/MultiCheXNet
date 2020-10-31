4feeec35b45d781fbd97c4bc315ecff1a8d4b58c
Abdullah Tarek <abdullahtarek@rocketmail.com>
2020-10-31 15:30:37 +0200

    Add report generation

--- data_loader/MTL_dataloader.py
+++ data_loader/MTL_dataloader.py
@@ -4,25 +4,29 @@ import tensorflow
 import numpy as np
 from .SIIM_ACR_dataloader import get_train_validation_generator as segmenatation_get_train_validation_generator
 from .RSNA_dataloader import get_train_validation_generator as detection_get_train_validation_generator
+from .indiana_dataloader import get_train_validation_generator as report_gen_get_train_validation_generator
 
 class MTL_generatot(tensorflow.keras.utils.Sequence):
     'Generates data from a Dataframe'
 
-    def __init__(self, Segmentation_gen, detection_gen , nb_iteration,batch_size,gen_type):
+    def __init__(self, Segmentation_gen, detection_gen , report_gen,  nb_iteration,batch_size,gen_type):
         'Initialization'
 
         self.seg_generator = Segmentation_gen
         self.det_generator = detection_gen
+        self.report_gen = report_gen
         
         self.gen_type=gen_type
         self.first_flag=0
         
         self.seg_itterator = self.seg_generator.__iter__()
         self.det_itterator = self.det_generator.__iter__()
+        if self.report_gen is not None:
+            self.report_itterator = self.report_gen.__iter__()
         
         self.nb_iteration = nb_iteration
         self.batch_size = batch_size
-        self.batch_number = 0
+        self.batch_number = 1
 
     def __len__(self):
         'Denotes the number of batches per epoch'
@@ -39,7 +43,7 @@ class MTL_generatot(tensorflow.keras.utils.Sequence):
         'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
         # Initialization
         if self.gen_type=='train':
-            if self.batch_number % 2 == 0:
+            if self.batch_number == 1:
                 try:
                     X, Y_seg = next(self.seg_itterator)
                 except:
@@ -56,8 +60,11 @@ class MTL_generatot(tensorflow.keras.utils.Sequence):
                 Y_class = np.array(Y_class)
                 Y_seg= np.array(Y_seg)
                 Y_det = np.ones([self.batch_size,8,8,5,6])*-1
+                
+                X_report = np.ones([self.batch_size , self.report_gen.max_len])*-1
+                Y_report = np.ones([self.batch_size , self.report_gen.max_len ,self.report_gen.vocab_size ])*-1
 
-            else:
+            if self.batch_number == 2:
                 try:
                     X, Y_det = next(self.det_itterator)
                 except:
@@ -73,8 +80,26 @@ class MTL_generatot(tensorflow.keras.utils.Sequence):
                 Y_class = np.array(Y_class)
                 Y_det= np.array(Y_det)
                 Y_seg = np.ones([self.batch_size,256,256,1])*-1
-
+                X_report = np.ones([self.batch_size , self.report_gen.max_len])*-1
+                Y_report = np.ones([self.batch_size , self.report_gen.max_len ,self.report_gen.vocab_size ])*-1
+            
+                if self.report_gen is None:
+                    self.batch_number =0 
+                
+            if self.batch_number == 3:
+                try:
+                    (X,X_report), Y_report = next(self.report_itterator) 
+                except:
+                    self.report_itterator = self.report_gen.__iter__()
+                    (X,X_report), Y_report = next(self.report_itterator) 
+                
+                Y_class = np.ones(self.batch_size)*-1 
+                Y_det = np.ones([self.batch_size,8,8,5,6])*-1
+                Y_seg = np.ones([self.batch_size,256,256,1])*-1
+                
+                self.batch_number =0
             self.batch_number += 1
+        
         
         if self.gen_type=='val':
             if self.first_flag==0:
@@ -109,14 +134,15 @@ class MTL_generatot(tensorflow.keras.utils.Sequence):
                 except:
                     self.first_flag=0
                     self.det_itterator = self.det_generator.__iter__()
-   
+                    
+        if self.report_gen is None:
+            return X, [Y_class, Y_det, Y_seg]
+        else:
+            return X, [Y_class, Y_det, Y_seg , X_report , Y_report]
 
-        return X, [Y_class, Y_det, Y_seg]
-
-def get_train_validation_generator(det_csv_path,seg_csv_path , det_img_path, seg_img_path ,batch_size=8, dim=(256,256), n_channels=3,
+def get_train_validation_generator(det_csv_path,seg_csv_path , det_img_path, seg_img_path, report_csv_path1=None,report_csv_path2=None,report_img_path=None, batch_size=8, dim=(256,256), n_channels=3,
                   shuffle=True ,preprocess = None , only_positive=True, validation_split=0.2,augmentation=False,normalize=False,hist_eq=False,batch_positive_portion=None ):
-
-
+    
 
 
     seg_train_gen, seg_valid_gen = segmenatation_get_train_validation_generator(seg_csv_path, seg_img_path,
@@ -139,8 +165,31 @@ def get_train_validation_generator(det_csv_path,seg_csv_path , det_img_path, seg
                                                                                 hist_eq=hist_eq,
                                                                                 validation_split=validation_split,batch_positive_portion=batch_positive_portion)
 
-    MTL_train_gen = MTL_generatot(seg_train_gen, det_train_gen , seg_train_gen.nb_iteration+det_train_gen.nb_iteration ,batch_size=batch_size , gen_type='train')
-    MTL_valid_gen = MTL_generatot(seg_valid_gen, det_valid_gen, seg_valid_gen.nb_iteration + det_valid_gen.nb_iteration , batch_size=batch_size, gen_type='val')
+    if report_csv_path1 is not None :
+        vocab_size=10000
+        max_len=100
+        shuffle_GT_sentences=True
+        feat_model=None
+        report_train_gen, report_valid_gen =  get_train_validation_generator(report_csv_path1,report_csv_path2,
+                                       report_img_path, vocab_size,max_len,
+                                       batch_size=batch_size, dim=dim,
+                                       shuffle=shuffle , preprocess = preprocess ,
+                                       validation_split=validation_split,augmentation=augmentation,
+                                       normalize=normalize,hist_eq =hist_eq,
+                                       shuffle_GT_sentences=shuffle_GT_sentences ,
+                                       feat_model=feat_model,over_sample=batch_positive_portion)
+        
+        MTL_train_gen = MTL_generatot(seg_train_gen, det_train_gen ,report_train_gen,
+                                      seg_train_gen.nb_iteration+det_train_gen.nb_iteration +report_train_gen.nb_iteration, 
+                                      batch_size=batch_size , gen_type='train')
+        
+        MTL_valid_gen = MTL_generatot(seg_valid_gen, det_valid_gen,report_valid_gen,
+                                      seg_valid_gen.nb_iteration + det_valid_gen.nb_iteration+ report_valid_gen.nb_iteration,
+                                      batch_size=batch_size, gen_type='val')
+    
+    else:
+        MTL_train_gen = MTL_generatot(seg_train_gen, det_train_gen ,None, seg_train_gen.nb_iteration+det_train_gen.nb_iteration ,batch_size=batch_size , gen_type='train')
+        MTL_valid_gen = MTL_generatot(seg_valid_gen, det_valid_gen, None, seg_valid_gen.nb_iteration + det_valid_gen.nb_iteration , batch_size=batch_size, gen_type='val')
 
     return MTL_train_gen, MTL_valid_gen
 
